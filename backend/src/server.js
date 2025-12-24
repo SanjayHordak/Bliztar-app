e521d296e536768cbde32876e7dc61420c7b727c
SanjayHordak <sanjay604116@gmail.com>
2025-12-24 10:20:23 +0530

    commits done

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,14 +27,6 @@ app.get("/api/health",(req,res)=>{
     res.status(200).json({message:'Success'});
 });
 
-//make the app for deployment
-if(ENV.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'../../admin/dist')));
-
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../../admin","dist","index.html"));
-    })
-};
 const startServer = async() =>{
     await connectDB();
     app.listen(port,() =>{
