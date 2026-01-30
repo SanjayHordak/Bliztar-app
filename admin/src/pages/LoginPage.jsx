a18be6d6cf35c99818d4894e13d167d28b88053a
SanjayHordak <sanjay604116@gmail.com>
2026-01-30 10:47:05 +0530

    chumma commit

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -1,9 +1,18 @@
+import { SignalMediumIcon } from "lucide-react";
 
 
 export default function LoginPage(){
     return(
         <>
-        <div style={{display:"flex",alignItems:"center",textAlign:"center"}}>Login Page</div>
+        <div style={{display:"flex",alignItems:"center",textAlign:"center"}}>Login Page
+            <Signedout>
+                <SignIn/>
+            </Signedout>
+
+            <SignedIn>
+              <UserButton/>
+            </SignedIn>
+        </div>
         </>
     )
 }
