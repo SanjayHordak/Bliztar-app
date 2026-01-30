fdc518455f6c19d50fc8d6fb50be1e5d7ea54177
SanjayHordak <sanjay604116@gmail.com>
2026-01-30 11:03:39 +0530

    login cheyyande

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -4,15 +4,14 @@ import { SignalMediumIcon } from "lucide-react";
 export default function LoginPage(){
     return(
         <>
-        <div style={{display:"flex",alignItems:"center",textAlign:"center"}}>Login Page
-            <Signedout>
-                <SignedIn/>
-            </Signedout>
+        <div style={{display:"flex",alignItems:"center",textAlign:"center"}}>Login Page</div>
+        <SignedOut>
+            <SignInButton mode="model"/>
+        </SignedOut>
 
-            <SignedIn>
-              <UserButton/>
-            </SignedIn>
-        </div>
+        <SignedIn>
+            <UserButton/>
+        </SignedIn>
         </>
     )
 }
