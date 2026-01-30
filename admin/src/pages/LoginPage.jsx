3d76ec3dd4c9eddb306e8d14f435d8ef4dde2f17
SanjayHordak <sanjay604116@gmail.com>
2026-01-30 11:16:12 +0530

    done with issues

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -1,18 +1,18 @@
 import { SignalMediumIcon } from "lucide-react";
-
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/clerk-react'
 
 export default function LoginPage(){
     return(
         <>
         <div style={{display:"flex",alignItems:"center",textAlign:"center"}}>
             <h1>Login Page</h1>
-        <SignedOut>
-            <SignInButton mode="model"/>
-        </SignedOut>
+            <SignedOut>
+                <SignInButton mode="modal" />
+            </SignedOut>
 
-        <SignedIn>
-            <UserButton/>
-        </SignedIn>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </div>
         </>
     )
